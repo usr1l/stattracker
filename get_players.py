48041c826d3f41c71f3237e57a946a6996c3eb0d
tkny <zhtony0398@gmail.com>
2024-05-23 17:49:39 -0400

    class models

--- get_players.py
+++ get_players.py
@@ -2,20 +2,34 @@ from nba_api.stats.endpoints import commonallplayers
 import pandas as pd
 
 
-def get_nba_players_csv():
-    players = commonallplayers.CommonAllPlayers(is_only_current_season=1)
-    players_df = players.get_data_frames()[0]
-    players_df[['PERSON_ID', 'DISPLAY_FIRST_LAST']].to_csv(
-        'nba_players.csv', index=False)
+def csv_file_check(func):
+    def wrapper(self, *args, **kwargs):
+        if not self.csv_file:
+            return "No CSV file found"
+        return func(self, *args, **kwargs)
+    return wrapper
 
 
-def get_players_from_csv(csv_file):
-    def find_player_by_id(player_name):
+class NBAPlayers:
+    def __init__(self, csv_file):
+        self.csv_file = csv_file or None
         # Read the CSV file into a DataFrame
-        df = pd.read_csv(csv_file)
+        self.df = pd.read_csv(csv_file) if csv_file else None
+
+    def change_csv_file(self, csv_file):
+        self.csv_file = csv_file
+        self.df = pd.read_csv(csv_file)
+
+    def get_nba_players_csv(self):
+        players = commonallplayers.CommonAllPlayers(is_only_current_season=1)
+        players_df = players.get_data_frames()[0]
+        players_df[['PERSON_ID', 'DISPLAY_FIRST_LAST']].to_csv(
+            'nba_players.csv', index=False)
+
+    def find_player_by_id(self, player_name):
 
         # Search for the player name
-        result = df[df['DISPLAY_FIRST_LAST'].str.contains(
+        result = self.df[self.df['DISPLAY_FIRST_LAST'].str.contains(
             player_name, case=False, na=False)]
 
         # Check if the result is empty
@@ -23,4 +37,3 @@ def get_players_from_csv(csv_file):
             return f"No player found with the name: {player_name}"
         else:
             return result[['PERSON_ID', 'DISPLAY_FIRST_LAST']].to_dict(orient='records')
-    return find_player_by_id
--- get_statistics.py
+++ get_statistics.py
@@ -2,7 +2,6 @@ import pandas as pd
 from nba_api.stats.endpoints import playercareerstats
 
 
-def get_player_career_stats(player_id):
-    player_stats = playercareerstats.PlayerCareerStats(player_id=player_id)
-    player_stats_df = player_stats.get_data_frames()[0]
-    return player_stats_df
+class NBAStatistics:
+    def __init__(self, csv_file)
+    # def get_pl
