101a5519d37dda8162b84c9acf444ab2d1411cc9
tkny <zhtony0398@gmail.com>
2024-05-23 16:45:51 -0400

    tabulate

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from nba_api.stats.endpoints import playergamelog, playercareerstats, commonallplayers
-import matplotlib.pyplot as plt
-import time
 import numpy as np
 import pandas as pd
-import requests
-import re
-import json
 from bs4 import BeautifulSoup
 from get_players import get_nba_players_csv, get_players_from_csv
+from get_statistics import get_player_career_stats
 pd.set_option('display.max_columns', None)
 
 nba_players = get_players_from_csv('nba_players.csv')
-print(nba_players('LeBron James')[0]['PERSON_ID'])
+print(get_player_career_stats(nba_players('lebron james')[0]['PERSON_ID']))
---
+++ display_tables.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from tabulate import tabulate
+from get_statistics import get_player_career_stats
+
+
+def create_table(df):
+    pass
