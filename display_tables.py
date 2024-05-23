11c5b73bbf71139d768113c688f030c5960c9548
tkny <zhtony0398@gmail.com>
2024-05-23 16:50:23 -0400

    dependencies

--- display_tables.py
+++ display_tables.py
@@ -3,6 +3,6 @@ import pandas as pd
 from tabulate import tabulate
 from get_statistics import get_player_career_stats
 
-
-def create_table(df):
-    pass
+print(tabulate(get_player_career_stats(2544).head(),
+      headers='keys', tablefmt='pretty'))
+# def create_table(df):
