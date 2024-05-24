88ad9bc207a5b7ff5377855ca60ec0fb72c7e914
tkny <zhtony0398@gmail.com>
2024-05-24 06:17:43 -0400

    get players games n seasons bug

--- app.py
+++ app.py
@@ -3,21 +3,26 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 from get_players import get_nba_players_csv, NBA
-from get_statistics import get_player_career_stats, get_team_players_career_stats, get_player_last_games, get_player_seasons, get_last_n_games_against_team
+from get_statistics import NBAStats
 pd.set_option('display.max_columns', None)
 
 
 CURRENT_SEASON_PLAYERS = NBA('nba_players.csv')
+STATISTICS = NBAStats()
 
 tatum = CURRENT_SEASON_PLAYERS.get_player_by_name('jayson tatum')
-
-celtics_players = CURRENT_SEASON_PLAYERS.get_players_by_team('celtics')
-
+# celtics_players = CURRENT_SEASON_PLAYERS.get_players_by_team('celtics')
 # print(get_player_career_stats(tatum[0]))
 # print(list(celtics_players.values()))
 # print(get_team_players_career_stats(list(celtics_players.values())))
-# print(get_player_last_games(tatum[0], 20))
+# print(STATISTICS.get_player_last_games(tatum[0], 20))
 # print(CURRENT_SEASON_PLAYERS.get_id_by_name('lebron james'))
-print(get_last_n_games_against_team(CURRENT_SEASON_PLAYERS.get_id_by_player_name(
-    'lebron james'), CURRENT_SEASON_PLAYERS.get_team_abbreviation('celtics'), 10))
-# print(get_player_career_stats(nba_players('lebron james')[0]['PERSON_ID']))
+# print(STATISTICS.get_player_career_stats(
+#     CURRENT_SEASON_PLAYERS.get_id_by_player_name('lebron james')))
+# print(STATISTICS.get_last_n_games_against_team(CURRENT_SEASON_PLAYERS.get_id_by_player_name(
+# 'lebron james'), CURRENT_SEASON_PLAYERS.get_team_abbreviation('celtics'), 10))
+# print(tatum[0])
+# id = tatum[0]
+print(CURRENT_SEASON_PLAYERS.get_id_by_team_name('heat'))
+print(STATISTICS.get_player_games_last_n_seasons(
+    id, team_id=CURRENT_SEASON_PLAYERS.get_id_by_team_name('heat')))
--- get_statistics.py
+++ get_statistics.py
@@ -1,70 +1,96 @@
 import pandas as pd
 from nba_api.stats.endpoints import playercareerstats, playergamelog, TeamGameLogs, commonplayerinfo
+from datetime import datetime
 
 
-def get_player_career_stats(player_id):
-    player_stats = playercareerstats.PlayerCareerStats(
-        player_id=player_id)
-
-    # gets alll seasons, pick the first for current season
-    # print(player_stats.get_data_frames())
-    player_stats_df = player_stats.get_data_frames()[0]
-    return player_stats_df
-
-
-def get_team_players_career_stats(player_ids):
-    stats_df = []
-    for player_id in player_ids:
+class NBAStats:
+    def get_player_career_stats(self, player_id):
         player_stats = playercareerstats.PlayerCareerStats(
             player_id=player_id)
 
         # gets alll seasons, pick the first for current season
         # print(player_stats.get_data_frames())
         player_stats_df = player_stats.get_data_frames()[0]
-        stats_df.append(player_stats_df)
+        return player_stats_df
 
-    return stats_df
+    def get_team_players_career_stats(self, player_ids):
+        stats_df = []
+        for player_id in player_ids:
+            player_stats = playercareerstats.PlayerCareerStats(
+                player_id=player_id)
 
+            # gets alll seasons, pick the first for current season
+            # print(player_stats.get_data_frames())
+            player_stats_df = player_stats.get_data_frames()[0]
+            stats_df.append(player_stats_df)
 
-def get_player_last_games(player_id, num_games):
-    gamelog_regular = playergamelog.PlayerGameLog(
-        player_id=player_id, season='2023-24', season_type_all_star='Regular Season').get_data_frames()[0]
-    gamelog_playoffs = playergamelog.PlayerGameLog(
-        player_id=player_id, season='2023-24', season_type_all_star='Playoffs').get_data_frames()[0]
-    logs = pd.concat([gamelog_playoffs, gamelog_regular])
-    return logs.head(num_games)
+        return stats_df
 
+    def get_player_last_games(self, player_id, num_games):
+        gamelog_regular = playergamelog.PlayerGameLog(
+            player_id=player_id, season='2023-24', season_type_all_star='Regular Season').get_data_frames()[0]
+        gamelog_playoffs = playergamelog.PlayerGameLog(
+            player_id=player_id, season='2023-24', season_type_all_star='Playoffs').get_data_frames()[0]
+        logs = pd.concat([gamelog_playoffs, gamelog_regular])
+        return logs.head(num_games)
 
-def get_player_seasons(player_id):
-    player_info = commonplayerinfo.CommonPlayerInfo(
-        player_id=player_id).get_data_frames()[0]
-    from_year = player_info['FROM_YEAR'].values[0]
-    to_year = player_info['TO_YEAR'].values[0]
-    return range(from_year, to_year + 1)
+    def get_player_seasons(self, player_id):
+        player_info = commonplayerinfo.CommonPlayerInfo(
+            player_id=player_id).get_data_frames()[0]
+        from_year = player_info['FROM_YEAR'].values[0]
+        to_year = player_info['TO_YEAR'].values[0]
+        return range(from_year, to_year + 1)
 
+    # def get_last_n_games_against_team(self, player_id, opponent_team_name, n):
+    #     # Fetch game logs for all available seasons
+    #     print(player_id, opponent_team_name, n)
+    #     all_game_logs = pd.DataFrame()
+    #     current_year = datetime.now().year
+    #     for i in range(current_year - 1, current_year - n - 1, -1):
+    #         season_str = f"{i}-{str(i + 1)[-2:]}"
+    #         gamelogs_regular = playergamelog.PlayerGameLog(
+    #             player_id=player_id, season=season_str, season_type_all_star='Regular Season').get_data_frames()[0]
+    #         gamelogs_playoffs = playergamelog.PlayerGameLog(
+    #             player_id=player_id, season=season_str, season_type_all_star='Playoffs').get_data_frames()[0]
+    #         game_logs = pd.concat([gamelogs_playoffs, gamelogs_regular])
+    #         if not game_logs.empty:
+    #             all_game_logs = pd.concat([all_game_logs, game_logs])
 
-def get_last_n_games_against_team(player_id, opponent_team_name, n):
-    # Fetch game logs for all available seasons
-    print(player_id, opponent_team_name, n)
-    all_game_logs = pd.DataFrame()
-    seasons = get_player_seasons(player_id)
-    for season in seasons:
-        season_str = f"{season}-{str(season + 1)[-2:]}"
-        game_logs = playergamelog.PlayerGameLog(
-            player_id=player_id, season=season_str).get_data_frames()[0]
-        all_game_logs = pd.concat([all_game_logs, game_logs])
+    #     print(all_game_logs)
 
-    print(all_game_logs)
+    #     # Filter the game logs to only include games against the specified opponent
+    #     games_against_team = all_game_logs[all_game_logs['MATCHUP'].str.contains(
+    #         f'vs. {opponent_team_name}|@ {opponent_team_name}', regex=True)]
 
-    # Filter the game logs to only include games against the specified opponent
-    games_against_team = all_game_logs[all_game_logs['MATCHUP'].str.contains(
-        f'vs. {opponent_team_name}|@ {opponent_team_name}', regex=True)]
+    #     if games_against_team.empty:
+    #         return "Empty"
 
-    if games_against_team.empty:
-        return "Empty"
+    #     # Sort by game date and get the last N games
+    #     games_against_team = games_against_team.sort_values(
+    #         by='GAME_DATE', ascending=False).head(n)
 
-    # Sort by game date and get the last N games
-    games_against_team = games_against_team.sort_values(
-        by='GAME_DATE', ascending=False).head(n)
-
-    return games_against_team
+    #     return games_against_team
+    def get_player_games_last_n_seasons(self, player_id, seasons=['2023-24', '2022-23', '2021-22'], team_id=None):
+        logs = pd.DataFrame()
+        for season in seasons:
+            try:
+                gamelog_regular = playergamelog.PlayerGameLog(
+                    player_id=player_id, season=season, season_type_all_star='Regular Season').get_data_frames()[0]
+                gamelog_playoffs = playergamelog.PlayerGameLog(
+                    player_id=player_id, season=season, season_type_all_star='Playoffs').get_data_frames()[0]
+                if team_id:
+                    if not gamelog_playoffs.empty:
+                        gamelog_playoffs = gamelog_playoffs[gamelog_playoffs['TEAM_ID'] == team_id]
+                    if not gamelog_regular.empty:
+                        gamelog_regular = gamelog_regular[gamelog_regular['TEAM_ID'] == team_id]
+                if not gamelog_playoffs.empty:
+                    if not gamelog_regular.empty:
+                        logs = pd.concat(
+                            [logs, gamelog_playoffs, gamelog_regular])
+                    else:
+                        logs = pd.concat([logs, gamelog_playoffs])
+                else:
+                    logs = pd.concat([logs, gamelog_regular])
+            except KeyError as e:
+                print(f"Error: {e}. Skipping season {season}.")
+        return logs
