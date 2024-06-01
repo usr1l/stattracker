a83a294a546f4a1811fe2e3c444d0d04efe3f8c5
tkny <zhtony0398@gmail.com>
2024-06-01 16:07:06 -0400

    latest

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from nba_api.stats.endpoints import playergamelog, playercareerstats, commonallplayers
-import numpy as np
 import pandas as pd
-from bs4 import BeautifulSoup
-from get_players import get_nba_players_csv, NBA
+from get_players import NBA
 from get_statistics import NBAStats
 from analyze_tables import Analysis
 from display_charts import DisplayCharts
@@ -12,7 +9,7 @@ pd.set_option('display.max_columns', None)
 CURRENT_SEASON = '2023-24'
 LAST_FIVE_SEASONS = ['2023-24', '2022-23', '2021-22', '2020-21', '2019-20']
 
-nba_players = NBA('nba_players.csv')
+nba_players = NBA('players_csv/nba_players.csv')
 nba_statistics = NBAStats()
 nba_charts = DisplayCharts()
 nba_analysis = Analysis()
