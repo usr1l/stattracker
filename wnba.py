6bc5b344870dc4870233cff5ac200d2fd5218028
tkny <zhtony0398@gmail.com>
2024-06-02 02:10:06 -0400

    beautiful soup;

--- wnba.py
+++ wnba.py
@@ -1,10 +1,24 @@
-from py_ball import league, image
+import requests
+from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
+url = "https://www.basketball-reference.com/wnba/players/c/clarkca02w/gamelog/2024/"
+response = requests.get(url)
 
-league_id = '10' # WNBA
-franchises = league.League(headers=headers,
-                           endpoint='franchisehistory',
-                           league_id=league_id)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-print(franchises)
+# print(soup)
+# # Extract player name
+player_name = soup.find('span', itemprop='name')
+print(player_name)
+
+# # Extract career statistics
+# career_stats_table = soup.find('table', id='per_game-playoffs_per_game')
+# career_stats_rows = career_stats_table.find_all('tr')
+
+# # for row in career_stats_rows:
+# #     cols = row.find_all('td')
+# #     if cols:
+# #         season = cols[0].text.strip()
+# #         games_played = cols[1].text.strip()
+# #         points_per_game = cols[2].text.strip()
+# #         print(f"Season: {season}, Games Played: {games_played}, Points Per Game: {points_per_game}")
