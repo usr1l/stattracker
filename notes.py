79de2a14784b8ec1f37c5ba110fd57bcc297829c
tkny <zhtony0398@gmail.com>
2024-06-02 02:38:19 -0400

    clarke csv

--- notes.py
+++ notes.py
@@ -19,3 +19,7 @@
 # print(gamelog_regular[gamelog_regular['MATCHUP'].str.contains(
 #     matchup, case=False, na=False)])
 # print(gamelog_regular.columns)
+
+
+# Rk,GAME_DATE,AGE,MATCHUP,WL,GS,MP,FG,FGA,FG%,FG3M,3PA,3P%,FT,FTA,FT%,ORB,DRB,TRB,AST,STL,BLK,TOV,PF,PTS,GmSc
+# SEASON_ID,Player_ID,Game_ID,GAME_DATE,MATCHUP,WL,MIN,FGM,FGA,FG_PCT,FG3M,FG3A,FG3_PCT,FTM,FTA,FT_PCT,OREB,DREB,REB,AST,STL,BLK,TOV,PF,PTS,PLUS_MINUS,VIDEO_AVAILABLE
